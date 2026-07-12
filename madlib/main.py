6b31594699afeb1db65ae87e882cf3e006cb8376
Pranamya <pranamyaks9@gmail.com>
2026-07-12 08:15:08 +0530

    Create main.py

---
+++ madlib/main.py
@@ -0,0 +1,13 @@
+'''In this python project i learned about string concatenation(how to put string together)
+to print concatenated string is there few ways
+youtuber="Kylie Ying"
+print("subscribe to"+youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber})'''
+adj=input("Adjective:")
+verb1=input("verb:")
+verb2=input("verb:")
+famous_person=input("Famous Person:")
+madlib=f"Computer programming is so {adj}! it makes me so excited all the time bz\ I love to code {verb1}.stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib) 
+
